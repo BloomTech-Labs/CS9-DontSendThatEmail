5c189f82dbe5e72ae0873e011af16a0c95e634ed
jaivonmassena <jaivonmassena@gmail.com>
2018-08-15 00:13:29 +0200

    signup and login boilerplate done

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Landing from './landing-page'
+import Login from './login'
+import Signup from './signup'
+import { Route } from 'react-router-dom'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Landing/>
+
+       <Route exact path="/" component= { Landing }/>
+       <Route path="/signup" component={ Signup }/>
+       <Route path="/login" component= { Login }/>
       </div>
     );
   }

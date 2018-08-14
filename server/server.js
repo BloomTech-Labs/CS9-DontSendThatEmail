70630dc9b6521edc8d73494613b3db93f8bf57e5
Velsu <riftmage50@gmail.com>
2018-08-14 19:53:00 +0200

    Added scaffold for server.js setup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
 // main server file
-
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
+const cors = require("cors");
+
+mongoose
+  .connect("mongodb://localhost/toneapp")
+  .then(() => console.log(`\n====  connected to mongo ====\n`))
+  .catch(() => console.log(`error connecting to mongo`));
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`\n=== API running on http://localhost:${PORT} ===\n`);
+});

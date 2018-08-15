f2cd009d3a7a7b92734aa723513d70ef6ef30745
Velsu <riftmage50@gmail.com>
2018-08-15 20:09:52 +0200

    Added register route to authRouter

--- server/api/routers/authRouter.js
+++ server/api/routers/authRouter.js
@@ -3,9 +3,12 @@ const router = require("express").Router();
 const { login, authenticate, localStrategy } = require("../controllers/login");
 const { protected, jwtStrategy } = require("../jwt/jwt");
 const passport = require("passport");
+const createUser = require("../controllers/createUser");
 
 passport.use(jwtStrategy);
 passport.use(localStrategy);
+
+router.route("/register").post(createUser);
 
 router.route("/login").post(authenticate, login);
 

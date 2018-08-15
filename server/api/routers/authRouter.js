0e70034736b56321e36d178ed274474d2dbb0aad
Velsu <riftmage50@gmail.com>
2018-08-15 20:07:51 +0200

    Added and configued auth Route for login the user

--- server/api/routers/authRouter.js
+++ server/api/routers/authRouter.js
@@ -1,1 +1,12 @@
 // place for auth router
+const router = require("express").Router();
+const { login, authenticate, localStrategy } = require("../controllers/login");
+const { protected, jwtStrategy } = require("../jwt/jwt");
+const passport = require("passport");
+
+passport.use(jwtStrategy);
+passport.use(localStrategy);
+
+router.route("/login").post(authenticate, login);
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,10 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const cors = require("cors");
-const db = require("./api/utils/config").mongoURI
+const db = require("./api/utils/config").mongoURI;
+const authRouter = require("./api/routers/authRouter");
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log(`\n====  connected to mongo ====\n`))
   .catch(() => console.log(`error connecting to mongo`));
 
@@ -14,6 +18,8 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
+
+server.use("/auth", authRouter);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {

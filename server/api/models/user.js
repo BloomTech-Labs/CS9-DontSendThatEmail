59d8ad44b3011c2afbd8de508e645b7843fa678a
Velsu <riftmage50@gmail.com>
2018-08-17 00:03:50 +0200

    Implemented protected routes for letters

--- server/api/models/user.js
+++ server/api/models/user.js
@@ -1,51 +1,51 @@
 // place for user model
-const mongoose = require('mongoose'); 
-const bcrypt = require('bcrypt');
-const SALT_ROUNDS = 11; 
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const SALT_ROUNDS = 11;
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 //User Model Schema
-const userSchema = mongoose.Schema ({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-    },
-    password: {
-        type: String,
-        required: true, 
-    },
-    letters: [
-        {
-            type: ObjectId,
-            ref: "Letter"
-        }
-    ]
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  letters: [
+    {
+      type: ObjectId,
+      ref: "Letter"
+    }
+  ]
 });
 
 //Add in Bcrypt for PW hashing
-userSchema.pre('save', function(next) {
-    bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
-        if (err) return next(err);
-        this.password = hash;
-        return next();
-    })
+userSchema.pre("save", function(next) {
+  bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    return next();
+  });
 });
 
-userSchema.methods.checkPassword = function (plainTextPW, callback) {
-    return bcrypt.compare(plainTextPW, this.password, function(err, isValid) {
-        if(err) {
-            return callback(err);
-        }
-        callback(null, isValid)
-    })
+userSchema.methods.checkPassword = function(plainTextPW, callback) {
+  return bcrypt.compare(plainTextPW, this.password, function(err, isValid) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, isValid);
+  });
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- server/api/routers/letterRouter.js
+++ server/api/routers/letterRouter.js
@@ -1,89 +1,94 @@
 // place for Letter0 router
 const router = require("express").Router();
+const User = require("../models/user");
+const { login, authenticate, localStrategy } = require("../controllers/login");
+const { protected, jwtStrategy } = require("../jwt/jwt");
+const passport = require("passport");
 
+const Letter = require("../models/letter");
 
-const Letter = require('../models/letter'); 
-
-
-router.get("/", (req, res) => {
-    Letter.find().then(resp => {
-        res.status(200).json(resp); 
-    })
-    .catch(err => {
-        res.status(500).json({
-            error: err
-        })
-    } )
-});
-
-
-router.get("/:id", (req, res) => {
-    const id = req.params.id; 
-    Letter.findById(id)
-    .populate('user_id')
+router.get("/", protected, (req, res) => {
+  const userid = req.user._id;
+  User.findById(userid)
+    .populate("letters")
     .then(resp => {
-        res.status(200).json(resp); 
+      res.status(200).json(resp);
     })
     .catch(err => {
-        res.status(500).json({
-            error: err
-        })
-    } )
+      res.status(500).json({
+        error: err
+      });
+    });
 });
-  
 
-
-router.post("/", (req, res) => {
-    const  letter = new Letter(req.body); 
-    // Letter.versions.push(letter); 
-    letter.save()
+router.get("/:id", protected, (req, res) => {
+  const id = req.params.id;
+  Letter.findById(id)
+    .populate("user_id")
     .then(resp => {
-        res.status(200).json(resp); 
+      res.status(200).json(resp);
     })
     .catch(err => {
-        res.status(500).json({
-            error: err
-        })
-    })
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
-
+router.post("/", protected, (req, res) => {
+  console.log(req.user._id);
+  const letter = new Letter({ ...req.body, user_id: req.user._id });
+  // Letter.versions.push(letter);
+  letter
+    .save()
+    .then(resp => {
+      res.status(200).json(resp);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+});
 
 // http://localhost:5000/letters/updateLetter
 
-router.post('/updateLetter', (req, res) => {
-    // console.log(req.body);
-    const { content, id } = req.body;
-      // Insert possible check for if `content` was provided here
-      Letter.findById(id).then(letter => {
-      letter.versions.push({ content });
-      letter.save()
-        .then(updatedletter => {
-          res.status(200).json(updatedletter);
-        })
-        .catch(err => {
-          res.status(404).json(err);
-        });
-    });
+router.post("/updateLetter", (req, res) => {
+  // console.log(req.body);
+  const { content, id } = req.body;
+  // Insert possible check for if `content` was provided here
+  Letter.findById(id).then(letter => {
+    letter.versions.push({ content });
+    letter
+      .save()
+      .then(updatedletter => {
+        res.status(200).json(updatedletter);
+      })
+      .catch(err => {
+        res.status(404).json(err);
+      });
+  });
 });
 
-router.delete('/:id', (req, res) => {
-   const id = req.params.id;
-   Letter.findByIdAndRemove(id)
-   .then(response => {
-        if (response.ok === 1) {
-            res.status(200).json({ message: `succussfully deleted the letter with id: ${id}` });
-        } else {
-            res.status(404).json({ message: "The letter with the specified ID does not exist." });
-        }
+router.delete("/:id", protected, (req, res) => {
+  const id = req.params.id;
+  Letter.findByIdAndRemove(id)
+    .then(response => {
+      if (response.ok === 1) {
+        res
+          .status(200)
+          .json({ message: `succussfully deleted the letter with id: ${id}` });
+      } else {
+        res.status(404).json({
+          message: "The letter with the specified ID does not exist."
+        });
+      }
     })
     .catch(err => {
-    res.status(500).json({ error: "The letter information could not be retrieved." });
+      res
+        .status(500)
+        .json({ error: "The letter information could not be retrieved." });
     });
-})
-
-
-
-
+});
 
 module.exports = router;

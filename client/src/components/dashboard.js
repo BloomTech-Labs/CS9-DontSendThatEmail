8b6788044f3ddaaa28526b2be554c9c456473432
Jacob Leon Lyerla <umasour@yahoo.com>
2018-08-15 19:07:21 -0700

    working on css for dashboard, looks pretty much done for the first demo

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
-import { Card, CardBody, Col } from "reactstrap";
+import { Card, CardTitle, Col,Row } from "reactstrap";
 import { Link } from "react-router-dom";
+import './dashboard.css'
 class Dashboard extends Component {
   constructor() {
     super();
@@ -11,14 +12,16 @@ class Dashboard extends Component {
       <Fragment>
         <Col md="3">
           <Card className="">
-            <CardBody>
-              <Link to="/documents">Documents</Link>
-              <br />
-              <Link to="/billing">Billing</Link>
-              <br />
-              <Link to="/dashboard/settings">Settings</Link>
-              <br />
-            </CardBody>
+        <CardTitle>Don't Send</CardTitle>
+              <Link  style={{ textDecoration: 'none',color:"white" }}  to="/documents"><div className="link-mod"><i class="far fa-file"></i><div >Documents</div></div></Link>
+            
+              <Link  style={{ textDecoration: 'none',color:"white" }} to="/billing"><div className="link-mod" ><i class="fas fa-money-bill-wave-alt"></i><div>Billing</div></div></Link>
+             
+              <Link  style={{ textDecoration: 'none',color:"white" }} to="/dashboard/settings"><div  className="link-mod"><i class="fas fa-cogs"></i>
+
+<div>Settings</div></div></Link>
+           
+           
           </Card>
         </Col>
       </Fragment>

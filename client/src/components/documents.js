b764051eaf8d00b3e20e7f9f64fe2bba4f4abb9e
Jacob Leon Lyerla <umasour@yahoo.com>
2018-08-21 16:59:14 -0700

    rendering data inside of documents, and removed some of the imports that we were not using

--- client/src/components/documents.js
+++ client/src/components/documents.js
@@ -1,52 +1,77 @@
-import React, { Component, Fragment } from "react";
+import React, { Component,Fragment } from "react";
 import {
   Row,
   Col,
   Card,
   CardBody,
   CardTitle,
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-  Input,
   Button,
-  Label,
   CardSubtitle,
   CardText
 } from "reactstrap";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./documents.css";
 import AddLetter from "./addLetter";
-import Document from "./document";
 class Documents extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      letters: []
+    };
+  }
+  componentDidMount() {
+    //let id = this.props.context.userData.id
+    
+    console.log("mounted",localStorage.getItem("token"))
+    axios
+      .get(`https://dontemail.herokuapp.com/letters`, {
+        headers: { "Authorization": localStorage.getItem("token") }
+      })
+      .then(resp => {
+        console.log(resp.data.letters);
+        this.setState({ letters: resp.data.letters });
+      }).catch(err =>{
+        console.log(err)
+      })
+
+  }
+ 
+  listDocuments() {
+    console.log("i ran list documents")
+    return this.state.letters.map(letter => (
+      <Fragment>
+      <Col md="4">
+        <Card className="documents-style">
+          <CardBody>
+            <CardTitle>{letter.name}</CardTitle>
+
+            <br />
+            <CardSubtitle>{letter.destination}</CardSubtitle>
+            <br />
+            <CardText>
+              {letter.versions[letter.versions.length -1].content}
+            </CardText>
+
+            <Link to="/dashboard/create">
+              <Button>Copy</Button>
+            </Link>
+          </CardBody>
+        </Card>
+      </Col>
+    </Fragment>));
   }
 
-  listDocuments() {
-    let i = 0;
-    let docs = [];
-    for (; i < 5; i++) {
-      docs.push(<Document key={i} />);
-    }
-    return (
-      <Row>
-        {docs}
-        <AddLetter {...this.props}/>
-      </Row>
-    );
-  }
-  addletter() {
-    return <Col md="9" />;
-  }
   render() {
     const { auth } = this.props.context.userData;
 
     return (
       <div>
         {auth ? (
-          <Col md="8">{this.listDocuments()}</Col>
+          <Col md="8">
+            <Row>{this.listDocuments()}</Row>
+            <AddLetter {...this.props} />
+          </Col>
         ) : (
           this.props.history.push("/")
         )}
--- client/src/contexts/authProvider.js
+++ client/src/contexts/authProvider.js
@@ -4,31 +4,33 @@ export const AuthContext = React.createContext({});
 class AuthProvider extends Component {
   state = {
     auth: false,
-    username: "test",
-    password: "pass",
-    letters: []
+    username: "",
+    id: "",
+    email: ""
   };
 
   toggleAuth = () => {
     this.setState({ auth: !this.state.auth });
   };
 
-  setLogin =(data) => {
+  setLogin = data => {
     this.setState({
-        auth: true, 
-        username: data.username ? data.username : '', 
-        password: data.password ? data.password : '', 
-        letters: data.letters ? data.letters : []
-    })
-  }
-
+      auth: true,
+      username: data.username ? data.username : "",
+      id: data._id ? data._id : "",
+      email: data.email ? data.email : ""
+    });
+  };
 
   render() {
-      const userData = this.state; 
+    const userData = this.state;
 
     return (
       <AuthContext.Provider
-        value={{ userData, actions: { toggleAuth: this.toggleAuth, setLogin: this.setLogin } }}
+        value={{
+          userData,
+          actions: { toggleAuth: this.toggleAuth, setLogin: this.setLogin }
+        }}
       >
         {this.props.children}
       </AuthContext.Provider>

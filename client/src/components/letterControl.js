229002dbf269e66cce389eaeee558ae68a3f4df1
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-08-22 14:48:29 -0700

    Added ability to create new note; routed to edit page

--- client/src/components/letterControl.js
+++ client/src/components/letterControl.js
@@ -7,7 +7,8 @@ import {
   Input,
   Row,
   Progress,
-  Label
+  Label,
+  Form
 } from "reactstrap";
 import "./letterControl.css";
 import axios from "axios";
@@ -20,6 +21,7 @@ class LetterControl extends Component {
       versions: [{ content: "Content..." }],
       name: "Name",
       destination: "To",
+      content: "",
       versionsCounter: 0,
       anger: 0,
       sadness: 0,
@@ -49,6 +51,31 @@ class LetterControl extends Component {
         this.versionsCounter();
       })
       .catch(err => {});
+  }
+
+  // allow user to create new letter
+  createLetter() {
+    let letter = {};
+    if (this.state.name !== "Name") {
+      letter.name = this.state.name;
+    }
+    if (this.state.destination !== "To") {
+      letter.destination = this.state.destination;
+    }
+    if (this.state.content !== "") {
+      letter.content = this.state.content;
+    }
+
+    // NOTE: Route not accepting destination info from user defaulting to N/A
+    axios
+      .post("https://dontemail.herokuapp.com/letters", letter, {
+        headers: { Authorization: localStorage.getItem("token") }
+      })
+      .then(resp => {
+        this.props.history.push(`/dashboard/create/${resp.data._id}`);
+        this.setletter(resp.data._id);
+      })
+      .catch(err => console.log(err));
   }
 
   // save the content of the current content
@@ -94,12 +121,21 @@ class LetterControl extends Component {
   }
   // render the save button based on the if the version is the most current version.
   renderSave() {
-    if (this.state.versionsCounter + 1 === this.state.versions.length) {
-      return <Button onClick={() => this.saveVersion()}>Save</Button>;
+    if (this.state.id === "") {
+      return <Button onClick={() => this.createLetter()}>Create</Button>;
     } else {
-      return <Button onClick={() => this.saveVersion()}>Save As</Button>;
+      if (this.state.versionsCounter + 1 === this.state.versions.length) {
+        return <Button onClick={() => this.saveVersion()}>Save</Button>;
+      } else {
+        return <Button onClick={() => this.saveVersion()}>Save As</Button>;
+      }
     }
   }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     const { auth } = this.props.context.userData;
     return (
@@ -108,78 +144,93 @@ class LetterControl extends Component {
           <Card className="controlCard-styles">
             <CardBody>
               <br />
-              <Row>
-                <Col md="3">
-                  <Input value={this.state.name} />
-                </Col>
-                <Col md="2">
-                  Edit {this.state.versionsCounter + 1}/
-                  {this.state.versions.length}
-                </Col>
-              </Row>
-              <br />
-              <Row>
-                <Col md="3">
-                  <Input value={this.state.destination} />
-                </Col>
-                <Col md="1">
-                  <Button>Analyze</Button>
-                </Col>
-              </Row>
-              <br />
-              <Row className="rowTextArea-styles">
-                <Col md="6">
-                  <Input
-                    className="controlTextarea-styles"
-                    type="textarea"
-                    value={
-                      this.state.versions[this.state.versionsCounter].content
-                    }
-                  />
-                </Col>
-                <Col md="3">
-                  <Label>Anger</Label>
+              <Form>
+                <Row>
+                  <Col md="3">
+                    <Input
+                      placeholder={this.state.name}
+                      name="name"
+                      value={this.state.name}
+                      onChange={this.handleChange}
+                    />
+                  </Col>
+                  <Col md="2">
+                    Edit {this.state.versionsCounter + 1}/
+                    {this.state.versions.length}
+                  </Col>
+                </Row>
+                <br />
+                <Row>
+                  <Col md="3">
+                    <Input
+                      placeholder={this.state.destination}
+                      name="destination"
+                      value={this.state.destination}
+                      onChange={this.handleChange}
+                    />
+                  </Col>
+                  <Col md="1">
+                    <Button>Analyze</Button>
+                  </Col>
+                </Row>
+                <br />
+                <Row className="rowTextArea-styles">
+                  <Col md="6">
+                    <Input
+                      className="controlTextarea-styles"
+                      type="textarea"
+                      placeholder={
+                        this.state.versions[this.state.versionsCounter].content
+                      }
+                      name="content"
+                      value={this.state.content}
+                      onChange={this.handleChange}
+                    />
+                  </Col>
+                  <Col md="3">
+                    <Label>Anger</Label>
 
-                  <Progress color="danger" value={this.state.anger}>
-                    25%
-                  </Progress>
-                  <br />
-                  <Label>Joy</Label>
+                    <Progress color="danger" placeholder={this.state.anger}>
+                      25%
+                    </Progress>
+                    <br />
+                    <Label>Joy</Label>
 
-                  <Progress color="success" value={this.state.joy}>
-                    25%
-                  </Progress>
-                  <br />
-                  <Label>Sadness</Label>
+                    <Progress color="success" placeholder={this.state.joy}>
+                      25%
+                    </Progress>
+                    <br />
+                    <Label>Sadness</Label>
 
-                  <Progress value={this.state.sadness}>25%</Progress>
-                </Col>
-              </Row>
-              <br />
-              <Row className="controlBtn-styles">
-                <Col md="6">
-                  <i
-                    className="fas fa-arrow-circle-left"
-                    onClick={() => this.changeVersion("down")}
-                  />
-                </Col>
-                <Col md="6">
-                  <i
-                    className="fas fa-arrow-circle-right"
-                    onClick={() => this.changeVersion("up")}
-                  />
-                </Col>
-              </Row>
-              <br />
-              <Row className="controlBtn-styles">
-                <Col md="6">
-                  <Link to="/dashboard">
-                    <Button>Cancel</Button>
-                  </Link>
-                </Col>
+                    <Progress placeholder={this.state.sadness}>25%</Progress>
+                  </Col>
+                </Row>
+                <br />
+                <Row className="controlBtn-styles">
+                  <Col md="6">
+                    <i
+                      className="fas fa-arrow-circle-left"
+                      onClick={() => this.changeVersion("down")}
+                    />
+                  </Col>
+                  <Col md="6">
+                    <i
+                      className="fas fa-arrow-circle-right"
+                      onClick={() => this.changeVersion("up")}
+                    />
+                  </Col>
+                </Row>
+                <br />
+                <Row className="controlBtn-styles">
+                  <Col md="6">
+                    <Link to="/dashboard">
+                      <Button>Cancel</Button>
+                    </Link>
+                  </Col>
 
-                <Col md="6">{this.renderSave()}</Col>
-              </Row>
+                  <Col md="6">{this.renderSave()}</Col>
+                </Row>
+              </Form>
             </CardBody>
           </Card>
         ) : (

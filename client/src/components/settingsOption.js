bf4bac533f6d594da4c2ce81d81ad1c400403188
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-08-23 16:33:47 -0700

    Added note to settingsOption for team reference

--- client/src/components/settingsOption.js
+++ client/src/components/settingsOption.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, CardBody, CardTitle, Button, Col, Row } from "reactstrap";
 import { Link } from "react-router-dom";
 
+//This will allow users to go into their settings and update their email & password credentials
 const Options = props => {
   console.log("some string", props.context);
 

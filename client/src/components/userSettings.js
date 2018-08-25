38273e733d6f02f3cc3b0e617adfd0cfa8329d77
imsdhk <jappleleaf@gmail.com>
2018-08-24 20:15:06 -0400

    implemented the conditional rendering based on the params passed in route

--- client/src/components/userSettings.js
+++ client/src/components/userSettings.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import { Card, CardBody, CardTitle, Button, Col, Input, Row } from "reactstrap";
 import { Link } from "react-router-dom";
 
+
 class UserSettings extends Component {
   constructor(props) {
     super(props);
@@ -12,9 +13,63 @@ class UserSettings extends Component {
 
 componentDidMount() {
   const option = this.props.match.params.type;
-    this.setState({option,})
+    this.setState({option})
 }
 
+returnHome =() =>{
+  this.setState({option:''})
+  this.props.history.push('/dashboard')
+}
+
+renderInput = () => {
+  if(this.state.option === 'password') {
+   return  (
+     <Fragment>
+      <Row>
+          <Col md="4">Old Password</Col>
+          <Col md="4">
+            <Input />
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col md="4">New Password</Col>
+          <Col md="4">
+            <Input />
+          </Col>
+        </Row>
+     </Fragment>
+    )
+  } else if (this.state.option === 'email') {
+   return (  
+   <Fragment>
+   <Row>
+        <Col md="4">Old Email</Col>
+        <Col md="4">
+          <Input />
+        </Col>
+      </Row>
+      <br />
+      <Row>
+        <Col md="4">New Email</Col>
+        <Col md="4">
+          <Input />
+        </Col>
+      </Row>
+   </Fragment>
+    )
+  } else {
+    return (
+    <Card>
+      <CardBody>
+        Loding Options...
+      </CardBody>
+    </Card>
+    )
+  }
+}
+
+  
   render() {
     const { auth } = this.props.context.userData;
     console.log(this.state.option)
@@ -25,33 +80,15 @@ componentDidMount() {
             <Card>
               <CardBody>
                 <br />
+                {this.renderInput()}
                 <Row>
-                  <Col md="4">Name</Col>
-                  <Col md="4">
-                    <Input />
-                  </Col>
-                </Row>
-                <br />
-                <Row>
-                  <Col md="4">Old Password</Col>
-                  <Col md="4">
-                    <Input />
-                  </Col>
-                </Row>
-                <br />
-                <Row>
-                  <Col md="4">New Password</Col>
-                  <Col md="4">
-                    <Input />
-                  </Col>
-
                   <br />
                   <br />
                   <Col md="12">
                     <Button>Save</Button>
                   </Col>
                 </Row>
-                <Link to="/dashboard"><Button>Home</Button></Link>
+                <Button onClick={() => this.returnHome()}>Home</Button>
               </CardBody>
             </Card>
           </Fragment>

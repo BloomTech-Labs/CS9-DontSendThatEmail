33e0dea9e3aa96475275e8182c4b16d8d1c4c3ea
Velsu <riftmage50@gmail.com>
2018-08-27 23:53:08 +0200

    Apply validators for updateUser route

--- server/api/controllers/updateUser.js
+++ server/api/controllers/updateUser.js
@@ -1,17 +1,24 @@
 const User = require("../models/user");
+const validateUpdateInput = require("../../validation/update");
+const validateUpdateEmail = require("../../validation/updateEmail");
+const validateUpdatePassword = require("../../validation/updatePassword");
 
 const updateUser = (req, res) => {
-  let { password } = req.body;
+  if (req.body.email && req.body.password) {
+    const { errors, isValid } = validateUpdateInput(req.body);
+    if (!isValid) return res.status(400).json(errors);
+  } else if (req.body.email) {
+    const { errors, isValid } = validateUpdateEmail(req.body);
+    if (!isValid) return res.status(400).json(errors);
+  } else {
+    const { errors, isValid } = validateUpdatePassword(req.body);
+    if (!isValid) return res.status(400).json(errors);
+  }
   User.findOne({ _id: req.user._id }, function(err, user) {
     if (err) {
       res.status(404).json(err.message);
     }
 
-    if (password && password.length > 4) {
-      user.password = password;
-    } else {
-      res.status(500).json({ error: "Password is too short or empty" });
-    }
     user.save(function(err, returnData) {
       if (err) {
         res.status(500).json(err);

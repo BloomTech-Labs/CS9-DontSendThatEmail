1addd499659c19e8f532289534ab298b52e57427
Velsu <riftmage50@gmail.com>
2018-08-28 21:55:34 +0200

    Refactored the code, fixed bugs, added unique validation

--- server/api/controllers/updateUser.js
+++ server/api/controllers/updateUser.js
@@ -4,19 +4,24 @@ const validateUpdateEmail = require("../../validation/updateEmail");
 const validateUpdatePassword = require("../../validation/updatePassword");
 
 const updateUser = (req, res) => {
-  if (req.body.email && req.body.password) {
-    const { errors, isValid } = validateUpdateInput(req.body);
-    if (!isValid) return res.status(400).json(errors);
-  } else if (req.body.email) {
-    const { errors, isValid } = validateUpdateEmail(req.body);
-    if (!isValid) return res.status(400).json(errors);
-  } else {
-    const { errors, isValid } = validateUpdatePassword(req.body);
-    if (!isValid) return res.status(400).json(errors);
-  }
+  const { email, password } = req.body;
   User.findOne({ _id: req.user._id }, function(err, user) {
     if (err) {
       res.status(404).json(err.message);
+    }
+
+    if (req.body.email && req.body.password) {
+      const { errors, isValid } = validateUpdateInput(req.body);
+      if (!isValid) return res.status(400).json(errors);
+      else (user.password = password), (user.email = email);
+    } else if (req.body.email) {
+      const { errors, isValid } = validateUpdateEmail(req.body);
+      if (!isValid) return res.status(400).json(errors);
+      else user.email = email;
+    } else {
+      const { errors, isValid } = validateUpdatePassword(req.body);
+      if (!isValid) return res.status(400).json(errors);
+      else user.password = password;
     }
 
     user.save(function(err, returnData) {
@@ -24,7 +29,7 @@ const updateUser = (req, res) => {
         res.status(500).json(err);
         return;
       }
-      res.send(user);
+      res.send(returnData);
     });
   });
 };

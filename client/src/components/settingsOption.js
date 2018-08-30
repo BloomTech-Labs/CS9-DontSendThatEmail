4e96f9e6a047cb2d40f76b4ec6556f57bf820c01
Jacob Leon Lyerla <umasour@yahoo.com>
2018-08-30 15:05:00 -0700

    fixed setting up component styles a bit

--- client/src/components/settingsOption.js
+++ client/src/components/settingsOption.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, CardBody, CardTitle, Button, Col, Row } from "reactstrap";
 import { Link } from "react-router-dom";
-
+import "./settings.css"
 //This will allow users to go into their settings and update their email & password credentials
 const Options = props => {
   console.log("some string", props.context);

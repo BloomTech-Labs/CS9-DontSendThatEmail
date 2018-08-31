5d7e9443dac364f402522c0ec044ba2a3c07d502
jaivonmassena <jaivonmassena@gmail.com>
2018-08-31 15:54:16 -0400

    fixed shaking on documents route

--- client/src/App.js
+++ client/src/App.js
@@ -51,7 +51,7 @@ class App extends Component {
                   /> */}
                 {/* </Col> */}
                 {/* </Row> */}
-                <Row>
+                <Row className="body-background">
 
                   <Route
                     path="/dashboard"

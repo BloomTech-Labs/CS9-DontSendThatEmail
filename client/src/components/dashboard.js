dc9ac8af531ddd9407115ef5ed24e56242e96d31
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-09-07 15:19:07 -0700

    Proofed dashboard.js

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { Card, CardTitle, Col, Nav, NavItem } from "reactstrap";
+import { Col, Nav } from "reactstrap";
 import { Link } from "react-router-dom";
 import "./dashboard.css";
 class Dashboard extends Component {
@@ -10,72 +10,63 @@ class Dashboard extends Component {
 
   render() {
     const { auth } = this.props.context.userData;
-    console.log(this.props);
-    // if (!auth){
-
-    // this.props.history.push('/')
-    // }
-
-    // console.log('props',   this.props)
-    // console.log(auth)
 
     return (
       <Col className="sidebar" lg="2">
         {auth ? (
           <Fragment>
-
-          <Nav vertical className="nav">
-            <Link
-              to="/dashboard" >
-              <div className="dash-logo"></div>
-            </Link>
-            <div className="avat">
-            <i className="fa fa-user" aria-hidden="true"></i>
-          </div>
-            <div className="user">Hello, { this.props.context.userData.username }</div>
-            <Link
-              style={{ textDecoration: "none", color: "white" }}
-              to="/dashboard"
-            >
-
-              <div className="link-mod documents">
-                <i className="far fa-file" />
-                <div>Documents</div>
+            <Nav vertical className="nav">
+              <Link to="/dashboard">
+                <div className="dash-logo" />
+              </Link>
+              <div className="avat">
+                <i className="fa fa-user" aria-hidden="true" />
               </div>
-            </Link>
-
-            <Link
-              style={{ textDecoration: "none", color: "white" }}
-              to="/dashboard/billing"
-            >
-              <div className="link-mod billing">
-                <i className="fas fa-money-bill-wave-alt" />
-                <div>Billing</div>
+              <div className="user">
+                Hello, {this.props.context.userData.username}
               </div>
-            </Link>
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/dashboard"
+              >
+                <div className="link-mod documents">
+                  <i className="far fa-file" />
+                  <div>Documents</div>
+                </div>
+              </Link>
 
-            <Link
-              style={{ textDecoration: "none", color: "white" }}
-              to="/dashboard/settings/options"
-            >
-              <div className="link-mod setting">
-                <i className="fas fa-cogs" />
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/dashboard/billing"
+              >
+                <div className="link-mod billing">
+                  <i className="fas fa-money-bill-wave-alt" />
+                  <div>Billing</div>
+                </div>
+              </Link>
 
-                <div>Settings</div>
-              </div>
-            </Link>
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/dashboard/settings/options"
+              >
+                <div className="link-mod setting">
+                  <i className="fas fa-cogs" />
 
-            <Link onClick={() => localStorage.removeItem("token")}
-              to="/"
-              style={{ textDecoration: "none", color: "white" }}
-            >
-              <div className="link-mod">
-                <i className="fa fa-power-off" />
-                <div>Log Out</div>
-              </div>
-            </Link>
-          </Nav>
+                  <div>Settings</div>
+                </div>
+              </Link>
 
+              <Link
+                onClick={() => localStorage.removeItem("token")}
+                to="/"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <div className="link-mod">
+                  <i className="fa fa-power-off" />
+                  <div>Log Out</div>
+                </div>
+              </Link>
+            </Nav>
           </Fragment>
         ) : (
           this.props.history.push("/")
@@ -84,4 +75,4 @@ class Dashboard extends Component {
     );
   }
 }
-export default Dashboard ;
+export default Dashboard;
